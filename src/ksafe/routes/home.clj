4a92189c15fd0dc2c952092b28d574b14b634f2e
Steve Shogren <steve.a.shogren@gmail.com>
2015-05-30 07:11:29 -0400

    Working through the tutorial with the cljs

--- src/ksafe/routes/home.clj
+++ src/ksafe/routes/home.clj
@@ -1,14 +1,35 @@
 (ns ksafe.routes.home
   (:require [ksafe.layout :as layout]
-            [compojure.core :refer [defroutes GET]]
+            [guestbook.db.core :as db]
+            [bouncer.core :as b]
+            [bouncer.validators :as v]
+            [ring.util.response :refer [redirect]]
+            [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :refer [ok]]
             [clojure.java.io :as io]))
 
-(defn home-page []
-  (layout/render "home.html"))
+(defn validate-message [params]
+  (first
+    (b/validate
+      params
+      :name v/required
+      :message [v/required [v/min-count 10]])))
+
+(defn save-message! [{:keys [params]}]
+  (if-let [errors (validate-message params)]
+    (-> (redirect "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/save-message! (assoc params :timestamp (java.util.Date.)))
+      (redirect "/"))))
+
+(defn home-page [{:keys [flash]}]
+  (layout/render
+   "home.html"
+   (merge {:messages (db/get-messages)}
+          (select-keys flash [:name :message :errors]))))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
-  (GET "/data" [] (ok (int (/ (.getTime (java.util.Date.)) 1000))))
-  (GET "/docs" [] (ok (-> "docs/docs.md" io/resource slurp))))
-
+  (GET "/" request (home-page request))
+  (POST "/" request (save-message! request))
+  (GET "/about" [] (about-page)))
